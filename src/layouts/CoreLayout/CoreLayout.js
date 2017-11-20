3aa9950416d894cc7b94b3dfc1ad2f56cf586e7b
Bohdan Polataiko <bp@se7ensky.com>
2017-11-20 10:37:02 +0200

    async routing tune

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -4,46 +4,41 @@ import {
 	Switch
 } from 'react-router-dom';
 import Loadable from 'react-loadable';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 import 'normalize.css/normalize.css';
 import 'styles/main.styl';
 
 
-const LoadableHeader = Loadable({
-	loader: () => import('components/Header'),
-	loading() {
-		return (<div>Loading...</div>);
+function Loading(props) {
+	if (props.error) {
+		return <div>Error!</div>;
+	} else if (props.pastDelay) {
+		return <div>Loading...</div>;
+	} else {
+		return null;
 	}
-});
-const LoadableFooter = Loadable({
-	loader: () => import('components/Footer'),
-	loading() {
-		return (<div>Loading...</div>);
-	}
-});
+}
 const LoadableHome = Loadable({
 	loader: () => import('routes/Home'),
-	loading() {
-		return (<div>Loading...</div>);
-	}
+	loading: Loading
 });
 const LoadableAbout = Loadable({
 	loader: () => import('routes/About'),
-	loading() {
-		return (<div>Loading...</div>);
-	}
+	loading: Loading
 });
 
 function CoreLayout() {
 	return (
 		<div className='app'>
-			<LoadableHeader />
+			<Header />
 			<div className='main'>
 				<Switch>
 					<Route exact path='/' component={LoadableHome} />
 					<Route exact path='/about-us' component={LoadableAbout} />
 				</Switch>
 			</div>
-			<LoadableFooter />
+			<Footer />
 		</div>
 	);
 }
