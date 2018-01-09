90f03654280952990a6fb019e6a9878fd560ce89
Taras Rodynenko <t.rodynenko@gmail.com>
2018-01-09 23:49:42 +0200

    connect redux-form to redux store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form';
 import counter from 'reducers/modules/counter';
 
 export default combineReducers({
 	router: routerReducer,
+	form: formReducer,
 	counter
 });
