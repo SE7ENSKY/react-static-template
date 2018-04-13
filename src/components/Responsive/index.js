114d5903a1d0c43c86ae2ef6109bc4ad71837847
Taras Rodynenko <t.rodynenko@gmail.com>
2018-04-13 14:57:35 +0300

    add Responsive components

---
+++ src/components/Responsive/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import MediaQuery from 'react-responsive';
+import { MIN_DESKTOP_WIDTH, MIN_TABLET_WIDTH } from 'store/constants';
+
+const Desktop = props => <MediaQuery minWidth={MIN_DESKTOP_WIDTH} {...props} />;
+const Tablet = props => (
+	<MediaQuery
+		minWidth={MIN_TABLET_WIDTH}
+		maxWidth={MIN_DESKTOP_WIDTH - 1}
+		{...props}
+	/>
+);
+const Mobile = props => <MediaQuery maxWidth={MIN_TABLET_WIDTH - 1} {...props} />;
+const TabletDesktop = props => <MediaQuery minWidth={MIN_TABLET_WIDTH} {...props} />;
+const MobileTablet = props => <MediaQuery maxWidth={MIN_DESKTOP_WIDTH - 1} {...props} />;
+
+export {
+	Desktop,
+	Tablet,
+	Mobile,
+	TabletDesktop,
+	MobileTablet
+};
--- src/store/constants.js
+++ src/store/constants.js
@@ -6,3 +6,7 @@ export const DECREMENT = 'DECREMENT';
 export const CONTACT_FORM = 'CONTACT_FORM';
 export const TEXT = 'TEXT';
 export const EMAIL = 'EMAIL';
+
+// window media breakpoints
+export const MIN_DESKTOP_WIDTH = 1000;
+export const MIN_TABLET_WIDTH = 768;
