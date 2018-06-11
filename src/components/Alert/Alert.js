97d4d3179af2e2c2a19f4bf7112c57688b5d846c
Bohdan Polataiko <bp@se7ensky.com>
2018-06-11 15:01:57 +0300

    add Alert

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { string } from 'prop-types';
+import classNames from 'classnames';
+import './Alert.styl';
+
+
+function Alert(props) {
+	const {
+		modifier,
+		text
+	} = props;
+
+	return (
+		<div
+			className={classNames('alert', { [modifier]: !!modifier })}
+			role='alert'
+		>
+			{text}
+		</div>
+	);
+}
+
+Alert.propTypes = {
+	modifier: string,
+	text: string
+};
+
+Alert.defaultProps = {
+	modifier: 'alert-danger',
+	text: 'Ой, что-то пошло не так. Пожалуйста, перезагрузите страницу.'
+};
+
+export default Alert;
