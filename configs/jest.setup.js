2e42bbe2cbfa9455d3dc240eed639cb2895719b2
Bohdan Polataiko <bp@se7ensky.com>
2018-07-29 11:01:13 +0300

    tune jest

--- configs/jest.setup.js
+++ configs/jest.setup.js
@@ -1,3 +1,4 @@
+import jsdom from 'jsdom';
 import sinon from 'sinon';
 import {
 	shallow,
@@ -10,8 +11,18 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-global.sinon = sinon;
+const { JSDOM } = jsdom;
+const page = '<!doctype html><html><head></head><body></body></html>';
+const {
+	document,
+	document: { defaultView }
+} = (new JSDOM(page)).window;
 
+global.document = document;
+global.window = defaultView;
+global.sinon = sinon;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+
+window.scrollTo = () => { };
